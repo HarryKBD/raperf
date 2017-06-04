2eb6e5da60b0316e57e4a17a8248d6289793d90f
Harry ByungDeok Kim <byungdeok.kim@gmail.com>
2017-06-03 16:58:03 -0900

    adding test code

--- app/raw.cpp
+++ app/raw.cpp
@@ -11,22 +11,35 @@
 using namespace std;
 
 
-char buf[1024];
-int buf_len = 1024;
+char FRAME_MAGIC[] = {0x33, 0x44, 0x55, 0x44, 0x44, 0x77, 0x77, 0x27};
+int MAGIC_LEN  = sizeof(FRAME_MAGIC);
+char temp_frame[8];
+
+
+char buf[1024*1024];
+int buf_len = 1024*1024;
 
 int main(int argc, char * argv[])
 {
     int len;
+    int framecount = 1000;
 
-    //if(prepare_connection("192.168.10.100", "9000") == 0){
-    if(prepare_connection("127.0.0.1", "9000") == 0){
+    if(prepare_connection("192.168.10.101", "9000") == 0){
+    //if(prepare_connection("127.0.0.1", "9000") == 0){
         cout << " Connection error" << endl;
         return 0;
     }
     
-    len = send_to_server(buf, buf_len);
+    while(framecount-- > 0){
+        len = send_to_server((char *)FRAME_MAGIC, MAGIC_LEN);
+        cout << "sent bytes: " << len << endl;
+        len = send_to_server(buf, buf_len);
+        cout << "sent bytes: " << len << endl;
+        usleep(10000);
+    }
 
-    cout << "sent bytes: " << len << endl;
+    char ch = getchar();
+    cout << "done" << endl;
 
     clean_transmission();
     return 1;
